b182515799506b0138b492b3e5d41d086f207f74
sumanta9090 <sumanta9090@gmail.com>
2023-12-03 03:54:09 +0530

    - Updated master

--- js/Application.js
+++ js/Application.js
@@ -63,7 +63,7 @@ define(
 					auth: {
 						clientId: "f06cc2c2-be11-4a2e-9db7-f4c27147cc0e",
 						authority: "https://login.microsoftonline.com/7266b166-c08a-4bdf-babd-868d05984b80",
-						redirectUri: "/"
+						redirectUri: "http://127.0.0.1/atlas/#/home"
 					}
 				};
 
